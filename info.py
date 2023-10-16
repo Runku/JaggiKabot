430fbf3b5aa062a3e8d7a1f506bcdb9445719a66
Runku <143589139+Runku@users.noreply.github.com>
2023-10-16 08:58:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -80,8 +80,8 @@ CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 # Other
 IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE)
 FILE_CAPTION = environ.get("FILE_CAPTION", script.FILE_CAPTION)
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "mdiskshortner.link")
-SHORTLINK_API = environ.get("SHORTLINK_API", "36f1ae74ba1aa01e5bd73bdd0bc22aa915443501")
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "https://tnshort.net")
+SHORTLINK_API = environ.get("SHORTLINK_API", "ad3def4886a08b8b396b452d08323f0d4595d276)
 WELCOME_TEXT = environ.get("WELCOME_TEXT", script.WELCOME_TEXT)
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/BackupByJaggi")
 
